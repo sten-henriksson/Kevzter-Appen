c2f5577f22440feb7ccd9d7581c7b7b61b40c621
Unknown <mangastens@gmail.com>
2018-09-20 20:35:48 +0200

    Working navdrawer with fragments and menu button

--- app/src/main/java/com/example/oauthtest/kevzterfinal/MainActivity.java
+++ app/src/main/java/com/example/oauthtest/kevzterfinal/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.oauthtest.kevzterfinal;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout mDrawerLayout;
+    private Fragment fragment = null;
+    Class fragmentClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        Log.i("INTENTINTENTINTENTINTE",menuItem.toString());
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        if(menuItem.getItemId()==R.id.menu1){
+                            fragmentClass = fragment1.class;
+                        }
+                        else if(menuItem.getItemId()==R.id.menu2){
+                            fragmentClass = fragment2.class;
+                        }
+                        else if(menuItem.getItemId()==R.id.menu3){
+                            fragmentClass = fragment3.class;
+                        }
+                        else if(menuItem.getItemId()==R.id.menu4){
+                            fragmentClass = fragment4.class;
+                        }
+
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+                        return true;
+                    }
+                });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
